6e957f392a3e4fd70c5ed0f0aaade67c110df7f8
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-12-02 17:33:49 +0300

    Task2

---
+++ Tyuiu.GoogeRA.Sprint6.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GoogeRA.Sprint6.Task2.V19.Lib;
+
+namespace Tyuiu.GoogeRA.Sprint6.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double[] TestArray = new double[] { 1.18, -14.95, -16.13, -7.17, -1.3, 0, -5.62, -0.52, 7.63, 5.97, -10.17 };
+            CollectionAssert.AreEqual(TestArray, ds.GetMassFunction(-5, 5));
+        }
+    }
+}
---
+++ Tyuiu.GoogeRA.Sprint6.Task2.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoogeRA.Sprint6.Task2.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoogeRA.Sprint6.Task2.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14719210-c2e6-42bd-8699-0ff52b68d1fc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
